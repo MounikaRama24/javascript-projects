a9a740da1122e2b525dc3c955122601fc2fd4572
akhil486 <akhildevopsaws@gmail.com>
2023-03-02 14:06:45 -0800

    first version

---
+++ colour flipper/colourFlipper.js
@@ -0,0 +1,12 @@
+// const coloursArray=['red','pink','blue','purple',"orange",'Green','black'];
+// const btn=document.getElementById('btn');
+// const color =document.querySelector('.color');
+// btn.addEventListener("click",function(){
+//      const randomNumber=getRandomNumber();
+//     document.body.style.backgroundColor=coloursArray[randomNumber];
+//     color.innerHTML=coloursArray[randomNumber];
+
+// });
+// function getRandomNumber(){
+//     return Math.floor(Math.random()*coloursArray.length);
+// }
